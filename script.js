98b0d80cf91d896e2e9cbb7ac7005c75736aa1a8
Kieran Savory <96089380+kieransavory@users.noreply.github.com>
2022-10-02 20:16:12 +0100

    Mobile functionality.
    Began coding mobile functionality.
    
    TO DO - 
    
    Create mobile navbar and relay to customer.

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
-function bgChanger() {
+// Functions to change background via selector icons on hero section of landing page. 
 
+function bgChangerOne() {
 
     let buttonOne = document.getElementById('heroButtonOne');
     let buttonTwo = document.getElementById('heroButtonTwo');
     let buttonThree = document.getElementById('heroButtonThree');
     let heroOne = document.getElementById('heroOne')
 
-    buttonOne.addEventListener('click', () => {
         heroOne.style.backgroundImage = "url(DBSheader2.jpg)";
         buttonOne.style.backgroundColor = "navy";
         buttonTwo.style.backgroundColor = "white";
         buttonThree.style.backgroundColor = "white";
-    })
+}
 
-    buttonTwo.addEventListener('click', () => {
-        heroOne.style.backgroundImage = "url(dbskitchen.jpg)"; 
+function bgChangerTwo() {
+
+    let buttonOne = document.getElementById('heroButtonOne');
+    let buttonTwo = document.getElementById('heroButtonTwo');
+    let buttonThree = document.getElementById('heroButtonThree');
+    let heroOne = document.getElementById('heroOne')
+    
+        heroOne.style.backgroundImage = "url(dbskitchen.jpg)";
         buttonOne.style.backgroundColor = "white";
         buttonTwo.style.backgroundColor = "navy";
         buttonThree.style.backgroundColor = "white";
-    })
 
-    buttonThree.addEventListener('click', () => {
+}
+
+function bgChangerThree() {
+
+    let buttonOne = document.getElementById('heroButtonOne');
+    let buttonTwo = document.getElementById('heroButtonTwo');
+    let buttonThree = document.getElementById('heroButtonThree');
+    let heroOne = document.getElementById('heroOne')
+    
         heroOne.style.backgroundImage = "url(dbsdesign.jpg)";
         buttonOne.style.backgroundColor = "white";
         buttonTwo.style.backgroundColor = "white";
         buttonThree.style.backgroundColor = "navy";
-    })
+
 }
+
+
+
+
+
+
+
